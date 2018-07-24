e4eb66326fd420b49309c580ceef272066cd0c1c
htoma <toma.horia@gmail.com>
2018-07-24 17:54:09 +0200

    Added Jun's fields to GifImage

--- BingApi/DbModel/GifImage.cs
+++ BingApi/DbModel/GifImage.cs
@@ -8,11 +8,14 @@ namespace BingApi.DbModel
     {
         public string Id { get; set; }
         public string ContentUrl { get; set; }
+        public string[] Keywords { get; set; }
+        public double Score { get; set; }
         public string ThumbnailUrl { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
         public string AccentColor { get; set; }
-        public string[] Keywords { get; set; }
-        public double Score { get; set; }
+        public string ContentSize { get; set; }
+        public string HostPageUrl { get; set; }
+        public string HostPageDisplayUrl { get; set; }
     }
 }
